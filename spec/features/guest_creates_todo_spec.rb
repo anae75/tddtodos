1b624a59a644a553c201ac9941584c5b7c327ee3
Ana <Ana@190.12.195.173.client.dyn.strong-in50.as13926.net>
2013-08-19 12:23:40 -0400

    refactor guest creates todo and abstract out some capybara

--- spec/features/guest_creates_todo_spec.rb
+++ spec/features/guest_creates_todo_spec.rb
@@ -1,13 +1,20 @@
 require  'spec_helper'
 
+def create_todo_named(name)
+  visit root_path
+  click_on 'Add new todo'
+  fill_in 'Name', with: name
+  click_on 'Submit'
+end
+
+def display_todo_named(name)
+  have_css('.todos li', text: name)
+end
+
 feature 'Guest creates todo' do 
   scenario 'create a todo item' do
-    visit root_path
-    click_on 'Add new todo'
-    fill_in 'Name', with: 'Buy milk'
-    click_on 'Submit'
+    create_todo_named 'Buy milk'
 
-    expect(page).to have_css('.todos li', text: 'Buy milk')
-    #puts current_path
+    expect(page).to display_todo_named 'Buy milk'
   end
 end
